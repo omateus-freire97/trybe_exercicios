45b87b16264476635025870a16c48e32fb709c35
omateus-freire97 <89813829+omateus-freire97@users.noreply.github.com>
2022-03-07 15:38:56 -0300

    Revert "Exercicios/4.1"

--- modulo1/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/exercicio1.js
+++
@@ -1,9 +0,0 @@
-const a = 8;
-const b = 5;
-
-
-console.log('Adição: ' + (a+b));
-console.log('Subtração: ' + (a-b));
-console.log('Multiplicação: ' + (a*b));
-console.log('Divisão: ' + (a/b));
-console.log('Módulo: ' + (a%b));
