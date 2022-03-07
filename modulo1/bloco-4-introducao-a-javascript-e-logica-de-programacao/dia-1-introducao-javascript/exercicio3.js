1e27c481b4334c4eab897efb74c6c1c9f80d8b68
Mateus Ferreira Freire <mateus.freire00@gmail.com>
2022-03-07 15:31:20 -0300

    exercicio 3

---
+++ modulo1/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/exercicio3.js
@@ -0,0 +1,11 @@
+const a = 5;
+const b = 8;
+const c = 12;
+
+if(a > b && a > c){
+    console.log("'a' é maior que 'b' e 'c'.");
+} else if(b > a && b > c){
+    console.log("'b' é maior que 'a' e 'c'.");
+} else{
+    console.log("'c' é maior que 'a' e 'b'.");
+}
