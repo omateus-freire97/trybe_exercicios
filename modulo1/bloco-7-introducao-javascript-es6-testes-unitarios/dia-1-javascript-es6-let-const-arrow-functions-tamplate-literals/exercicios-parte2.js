f478ea6a9529566cb4b4594464c2aaa69946f15d
Mateus Ferreira Freire <mateus.freire00@gmail.com>
2022-04-23 20:38:46 -0300

    parte 2 exercicio 2

--- modulo1/bloco-7-introducao-javascript-es6-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-tamplate-literals/exercicios-parte2.js
+++ modulo1/bloco-7-introducao-javascript-es6-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-tamplate-literals/exercicios-parte2.js
@@ -1,11 +1,29 @@
-// Crie uma função que receba um número e retorne seu fatorial.
+// 1 - Crie uma função que receba um número e retorne seu fatorial.
 
-const fatorial = (num) => {
-  let total = 1;
-  for (let i = 2; i <= num; i += 1) {
-    total = total * i;
+// const fatorial = (num) => {
+//   let total = 1;
+//   for (let i = 2; i <= num; i += 1) {
+//     total = total * i;
+//   }
+//   return total;
+// }
+
+// console.log(fatorial(5));
+
+// 2 - Crie uma função que receba uma frase e retorne qual a maior palavra.
+
+const recebeFrase = (frase) => {
+  let palavraFrase = frase.split(' ');
+  let maiorPalavra = 0;
+  let total = '';
+
+  for (let palavra of palavraFrase){
+    if (palavra.length > maiorPalavra) {
+      maiorPalavra = palavra.length;
+      total = palavra;
+    }
   }
   return total;
 }
 
-console.log(fatorial(5));
+console.log(recebeFrase('Antonio foi no banheiro e não sabemos o que aconteceu'));
